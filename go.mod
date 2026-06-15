663358efcb7fa0c83009f7361459713bde662baa
mthuwhihand <vnn159357@gmail.com>
2026-06-15 17:12:49 +0700

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test-secret-modules
+
+go 1.26.4
+
+require github.com/mthuwhihand/secret-modules v0.0.0-20260615091224-32aa8d7270ca
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import secret_modules "github.com/mthuwhihand/secret-modules"
+
+func main() {
+	println("Call secret process !")
+	secret_modules.SecretProcess()
+}
